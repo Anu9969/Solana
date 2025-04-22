b650c1c29cd06007c9701292590c745229d7a667
Anu9969 <sharmaanurag9969@gmail.com>
2025-04-23 02:25:19 +0530

    Implement process_instruction function for counter management

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub fn process_instructio(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) ->ProgramResult {
+    let account : &AccountInfo<'_> = next_account_info(&mut accounts.iter())?;
+    let mut counter:Counter = Counter::try_from_slice(&account.data.borrow())?;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    match CounterInstruction::try_from_slice(instruction_data)? {
+        CounterInstruction::Increment(amount:u32) => {
+            counter.count += amount;
+            msg!("Incremented counter by {}", amount);
+        }
+        CounterInstruction::Decrement(amount:u32) =>{
+            counter.count -= amount;
+            msg!("Decremented counter by {}", amount);
+        }
     }
+    // Serialize the updated counter back to the account data
+    counter.serialize(writer: &mut *account.data.borrow_mut())?;
+    msg!("Counter updated to {}", counter.count);
+    Ok(())
 }
